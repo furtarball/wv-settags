280eb8cb495c9da01578a459f3288e0322bd9ac2
furtarball <61846505+furtarball@users.noreply.github.com>
2020-03-13 18:26:25 +0100

    Add files via upload

---
+++ settags.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+printf "— settags 1.0 —\n\n"
+
+printf "Artist: "
+read artist
+printf "Album title: "
+read album
+printf "Original release year: "
+read date
+printf "Catalogue number: "
+read labelid
+
+wvtag -q -w "Artist=$artist" -w "Album=$album" -w "Year=$date" -w "Catalog=$labelid" *.wv
+
+printf "Writing track numbers…\n\n"
+declare -i tracknumber=0
+for i in *.wv;
+	do ((++tracknumber)) &&
+	if [ $tracknumber -le 9 ]
+		then
+		wvtag -q -w "Track=0$tracknumber" "$i"
+		mv "$i" "0$tracknumber.wv"
+	else
+		wvtag -q -w "Track=$tracknumber" "$i"
+		mv "$i" "$tracknumber.wv"
+	fi
+done
+
+for i in *.wv;
+	do printf "Track ${i::-3} title: " &&
+	read title &&
+	mv "$i" "${i::-3} – $title.wv";
+done
+
+printf "Done!\n"
